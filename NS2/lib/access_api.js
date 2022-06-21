13409ae75a2dbd870593124b6afbed6659f49c79
mmarkovic85 <milosmarkovic85@gmail.com>
2022-06-21 21:25:00 +0200

    NS2 (lib): added access_api.js

---
+++ NS2/lib/access_api.js
@@ -0,0 +1,40 @@
+export const create_access_api = async (ns) => {
+  // api methods
+  const gain_root = (target_hostname, target_req_open_port, attacker_hostname = ns.getHostname()) => {
+    switch (target_req_open_port) {
+      case 5:
+        if (ns.fileExists('SQLInject.exe', attacker_hostname)) {
+          ns.sqlinject(target_hostname);
+        }
+      case 4:
+        if (ns.fileExists('HTTPWorm.exe', attacker_hostname)) {
+          ns.httpworm(target_hostname);
+        }
+      case 3:
+        if (ns.fileExists('relaySMTP.exe', attacker_hostname)) {
+          ns.relaysmtp(target_hostname);
+        }
+      case 2:
+        if (ns.fileExists('FTPCrack.exe', attacker_hostname)) {
+          ns.ftpcrack(target_hostname);
+        }
+      case 1:
+        if (ns.fileExists('BruteSSH.exe', attacker_hostname)) {
+          ns.brutessh(target_hostname);
+        }
+      case 0:
+      default:
+        if (ns.fileExists('NUKE.exe', attacker_hostname)) {
+          ns.nuke(target_hostname);
+
+          return true;
+        }
+        else {
+          return false;
+        }
+    }
+  };
+
+  // api iface
+  return { gain_root };
+};
